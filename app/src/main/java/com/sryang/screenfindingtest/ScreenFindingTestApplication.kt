c8abace237b862b1a055dcb6d183d9dab2bec37d
Sarang Yang <>
2022-04-29 19:12:10 +0900

    update

---
+++ app/src/main/java/com/sryang/screenfindingtest/ScreenFindingTestApplication.kt
@@ -0,0 +1,8 @@
+package com.sryang.screenfindingtest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ScreenFindingTestApplication : Application() {
+}
