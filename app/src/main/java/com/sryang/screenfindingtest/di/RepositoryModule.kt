759447b36182a673a72b11a863c8ae2b407015cf
Sarang Yang <>
2022-05-02 17:51:57 +0900

    update

--- app/src/main/java/com/sryang/screenfindingtest/di/RepositoryModule.kt
+++ app/src/main/java/com/sryang/screenfindingtest/di/RepositoryModule.kt
@@ -11,6 +11,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -59,23 +60,39 @@ class TestFindRepositoryImpl @Inject constructor() : FindRepository {
     }
 
 
+    //최초 위치요쳥을 false로 설정 시 화면단에서 요청해야함
     val isFirstRequestLocation = MutableStateFlow(false)
     val isRequestingLocation = MutableStateFlow(false)
 
+    /**
+     * 화면 첫 진입 시 위치를 요청해야하는지에 대한 상태
+     */
     override fun getIsFirstRequestLocation(): StateFlow<Boolean> {
         return isFirstRequestLocation
     }
 
-    override suspend fun notifyRequestedLocation() {
+    /**
+     * 위치를 요청했다고 알려줌
+     */
+    override suspend fun notifyRequestLocation() {
         isFirstRequestLocation.emit(true)
         isRequestingLocation.emit(true)
+        //3초있다가 위치를 받은것으로 처리하는 테스트코드
+        delay(3000)
+        isRequestingLocation.emit(false)
     }
 
+    /**
+     * 현재 위치를 요청중인지 상태
+     */
     override fun isRequestingLocation(): StateFlow<Boolean> {
         return isRequestingLocation
     }
 
-    override suspend fun onReceiveLocation() {
+    /**
+     * 위치를 받았다고 알려줌
+     */
+    override suspend fun notifyReceiveLocation() {
         isRequestingLocation.emit(false)
     }
 
